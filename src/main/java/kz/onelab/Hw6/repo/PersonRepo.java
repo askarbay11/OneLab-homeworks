c2070ac17a9cf24785207dbc3be2fb8397df842f
Askar <170103110@stu.sdu.edu.kz>
2019-11-16 22:37:12 +0600

    elastic

--- src/main/java/kz/onelab/Hw6/repo/PersonRepo.java
+++ src/main/java/kz/onelab/Hw6/repo/PersonRepo.java
@@ -1,9 +1,10 @@
 package kz.onelab.Hw6.repo;
 
 import kz.onelab.Hw6.entity.Person;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PersonRepo extends CrudRepository<Person, Long> {
+public interface PersonRepo extends ElasticsearchRepository<Person, Long> {
     Iterable<Person> findByName(String name);
 
     Iterable<Person> findByNameEndingWith(String ending);
--- src/main/java/kz/onelab/Hw6/repo/PhoneRepo.java
+++ src/main/java/kz/onelab/Hw6/repo/PhoneRepo.java
@@ -1,8 +1,9 @@
 package kz.onelab.Hw6.repo;
 
 import kz.onelab.Hw6.entity.Phone;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PhoneRepo extends CrudRepository<Phone, Long> {
+public interface PhoneRepo extends ElasticsearchRepository<Phone, Long> {
     Iterable<Phone> findPhonesByPersonId(long personId);
 }
